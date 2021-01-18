962b2ed34e91a56f556f28a4c8d8bbd3a991eb89
nicopierro <pierro.nicolo@iisgalvanimi.edu.it>
2021-01-18 09:50:11 +0000

    angular-reddit

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -7,11 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    let t = title.value;
-    let v= link.value;
-    console.log(`Adding article title: `+t+ ` and link: `+ v);
-    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    return false;
+  return false;
   }
 }
+
